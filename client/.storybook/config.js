c3f87cf93c3c874590960caa7024843c54d51bea
leonp5 <leonpe@web.de>
2019-12-18 12:39:23 +0100

    Add postinstall script_change favicon_delete logo_add globalStyles

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -1,8 +1,19 @@
-import { configure, addDecorator } from "@storybook/react";
-import { withKnobs } from "@storybook/addon-knobs";
+import React from 'react';
+import GlobalStyles from '../src/GlobalStyles';
+import { configure, addDecorator } from '@storybook/react';
+import { withKnobs } from '@storybook/addon-knobs';
 
 // automatically import all files ending in *.stories.js
 
+// add GlobalStyle for every story
+const GlobalStyleDecorator = storyFn => (
+  <>
+    <GlobalStyles />
+    {storyFn()}
+  </>
+);
+
+addDecorator(GlobalStyleDecorator);
 addDecorator(withKnobs);
 
-configure(require.context("../src/stories", true, /\.stories\.js$/), module);
+configure(require.context('../src/stories', true, /\.stories\.js$/), module);
--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,9 @@
-import React from "react";
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
