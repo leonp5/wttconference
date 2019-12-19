c5ecb661c4a1fcd5c60b8f7b22f10c31c701306d
leonp5 <leonpe@web.de>
2019-12-19 17:52:27 +0100

    Modify MobileMenu

--- client/src/components/MobileMenu.js
+++ client/src/components/MobileMenu.js
@@ -1,15 +1,31 @@
 import React from "react";
 import styled from "@emotion/styled";
 
-const Container = styled.div`
+const Container = styled.nav`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: space-around;
-  height: 200px;
-  padding: 30px;
+  position: absolute;
+  height: 250px;
+  padding: 2rem;
+  top: 0;
+  right: 0;
+  transition: transform 0.3s ease-in-out;
   background: ${props => props.theme.colors.primary};
-  color: ${props => props.theme.colors.secondary};
+
+  a {
+    font-size: 1.5rem;
+    color: ${props => props.theme.colors.secondary};
+    text-decoration: none;
+    transition: color 0.3s linear;
+    margin: 5px;
+
+    &:hover {
+      background: ${props => props.theme.colors.secondary};
+      color: ${props => props.theme.colors.actioncolor};
+    }
+  }
 `;
 
 function MobileMenu({ children }) {
