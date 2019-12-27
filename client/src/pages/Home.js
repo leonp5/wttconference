c44ba9ae38c0e947e260abfccbfce010ec754dfc
leonp5 <leonpe@web.de>
2019-12-27 21:47:13 +0100

    Add comment to Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,23 +4,28 @@ import Button from "../components/Button";
 import Toggle from "../hooks/Toggle";
 import PopUpContent from "../components/PopUp/PopUpContent";
 import PopUpBackground from "../components/PopUp/PopUpBackground";
+import Heading from "../components/Heading";
 
 function Home() {
   return (
-    <p>
-      Click to reveal a secret:
-      <Toggle
-        toggle={show => <Button onClick={show}>Open</Button>}
-        content={hide => (
-          <PopUpBackground>
-            <PopUpContent>
-              <p>Vielen Dank für deine Anmeldung!</p>
-              <Button onClick={hide}>Close</Button>
-            </PopUpContent>
-          </PopUpBackground>
-        )}
-      />
-    </p>
+    <>
+      <Heading>Home</Heading>
+      {/* Thats for testing the PopUp and will removed after development */}
+      <p>
+        Click to reveal a secret:
+        <Toggle
+          toggle={show => <Button onClick={show}>Open</Button>}
+          content={hide => (
+            <PopUpBackground>
+              <PopUpContent>
+                <p>Vielen Dank für deine Anmeldung!</p>
+                <Button onClick={hide}>Close</Button>
+              </PopUpContent>
+            </PopUpBackground>
+          )}
+        />
+      </p>
+    </>
   );
 }
 
