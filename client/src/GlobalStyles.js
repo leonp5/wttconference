00ad5b79f3977662e60495d9c266015b0cdd270f
leonp5 <leonpe@web.de>
2019-12-27 14:28:46 +0100

    Implement font

--- client/src/GlobalStyles.js
+++ client/src/GlobalStyles.js
@@ -12,6 +12,14 @@ function GlobalStyles() {
         *:after {
           box-sizing: border-box;
         }
+        @font-face {
+          font-family: "Antropos";
+          src: url("/fonts/Antropos.eot");
+          src: url("/fonts/Antropos.eot?#iefix") format("embedded-opentype"),
+            url("/fonts/Antropos.otf") format("opentype"), url("/fonts/Antropos.svg") format("svg"),
+            url("/fonts/Antropos.ttf") format("truetype"),
+            url("/fonts/Antropos.woff") format("woff"), url("/fonts/Antropos.woff2") format("woff2");
+        }
 
         body {
           margin: 0;
