5907326ce568db08965f06a92f3b5633207cf784
leonp5 <leonpe@web.de>
2019-12-28 16:19:41 +0100

    Add Story for PopUp

---
+++ client/src/stories/6-PopUp.stories.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { text } from "@storybook/addon-knobs";
+
+import PopUpBackground from "../components/PopUp/PopUpBackground";
+import PopUpContent from "../components/PopUp/PopUpContent";
+
+export default {
+  title: "PopUp"
+};
+
+export function BasicPopUp() {
+  return (
+    <PopUpBackground>
+      <PopUpContent>{text("PopUp Content", "PopUp Content")}</PopUpContent>
+    </PopUpBackground>
+  );
+}
