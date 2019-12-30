c29e837bb1b0e235e810566701afc0cbc656e159
leonp5 <56833783+leonp5@users.noreply.github.com>
2019-12-30 14:34:59 +0100

    Add Hook for closing menu after click a link (#24)

--- client/src/components/AppBar.js
+++ client/src/components/AppBar.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "@emotion/styled";
 
 import useOnClickOutside from "../hooks/useOnClickOutside";
+import useOnClick from "../hooks/useOnClick";
 import BurgerButton from "./BurgerButton";
 import MobileMenu from "./MobileMenu";
 
@@ -27,6 +28,7 @@ function AppBar({ children }) {
   const [open, setOpen] = React.useState(false);
   const node = React.useRef();
   useOnClickOutside(node, () => setOpen(false));
+  useOnClick(() => setOpen(false));
   return (
     <Container ref={node}>
       <BurgerButton open={open} setOpen={setOpen} />
