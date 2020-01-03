0475bb98b91f18dfe36cce853f6548801b4db52c
leonp5 <56833783+leonp5@users.noreply.github.com>
2020-01-03 09:58:39 +0100

    Add server.js and database.js (#26)
    * Add server.js and database.js
    Setup local MongoDB
    
    * Setup routes
    Add post and get function
    
    * Modify Registration Page
    Start preparing to implement the api
    
    * Remove async from lib/attendees.js

---
+++ server.js
@@ -0,0 +1,28 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
+
+const { initDb } = require("./lib/database");
+const router = require("./lib/routes");
+
+const app = express();
+
+app.use(express.json({ extended: false }));
+
+app.get("/", (request, response) => {
+  response.send();
+});
+
+app.post("/", (request, response) => {
+  response.end();
+});
+
+initDb(process.env.MONGO_URL, process.env.DB_Name).then(() => {
+  console.log("Database steht");
+});
+
+app.use("/api", router);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on http://localhost:${process.env.PORT}`);
+});
