8f91c090c6f54bcc66531c0fc28c3d66d8ef421f
leonp5 <leonpe@web.de>
2020-01-03 11:16:44 +0100

    Replace json server functionality of form with mongoDB

--- client/src/api/attendees.js
+++ client/src/api/attendees.js
@@ -1,21 +1,11 @@
-function saveAttendee(attendee) {
+export function saveAttendee(attendee) {
   return fetch(`/api/attendees`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
     },
     body: JSON.stringify({
-      name: attendee.name,
-      address: attendee.address,
-      location: attendee.location,
-      email: attendee.email,
-      phone: attendee.phone,
-      workshop1: attendee.workshop1,
-      workshop2: attendee.workshop2,
-      workshop3: attendee.workshop3,
-      else: attendee.else
+      ...attendee
     })
   }).then(response => response.json());
 }
-
-export default saveAttendee;
