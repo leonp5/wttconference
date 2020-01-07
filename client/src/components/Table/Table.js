c5f5fc7dcabe9b2fd3fb40fe332398e6c0f38ae0
leonp5 <leonpe@web.de>
2020-01-07 15:22:34 +0100

    Add correct overflow to table elements

--- client/src/components/Table/Table.js
+++ client/src/components/Table/Table.js
@@ -5,6 +5,7 @@ import styled from "@emotion/styled";
 // const mq = breakpoints.map(bp => `@media (max-width: ${bp}px)`);
 
 export const Table = styled.table`
+  table-layout: fixed;
   width: 100%;
   border-collapse: collapse;
 `;
@@ -13,6 +14,8 @@ export const TableHeader = styled.th`
   padding: 5px;
   border-bottom: 1px solid ${props => props.theme.colors.tertiary};
   letter-spacing: 1px;
+  overflow: hidden;
+  text-overflow: ellipsis;
 `;
 
 export const TableHeaderRow = styled.tr``;
@@ -30,4 +33,6 @@ export const TableData = styled.td`
   color: ${props => props.theme.colors.font};
   padding: 5px;
   letter-spacing: 1px;
+  overflow: hidden;
+  text-overflow: ellipsis;
 `;
