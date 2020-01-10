f024a0c299d2c17ce1b798eea0070ebc379a2e18
leonp5 <leonpe@web.de>
2020-01-10 11:15:39 +0100

    Modify theme of nav links

--- client/src/components/Navigation/FooterNav.js
+++ client/src/components/Navigation/FooterNav.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "@emotion/styled";
-import { NavLink } from "./NavLink";
+import { BottomLink, FooterLink } from "./NavLink";
 
 const Footer = styled.footer`
   flex-grow: 0;
@@ -19,24 +19,12 @@ const AnchorWrapper = styled.div`
   justify-content: space-around;
 `;
 
-const Anchor = styled(NavLink)`
-  font-size: 1.1rem;
-  padding: 10px;
-  margin: 0;
-`;
-const BottomLink = styled(NavLink)`
-  color: ${props => props.theme.colors.secondary};
-  font-size: 0.8rem;
-  padding: 6px;
-  margin: 0;
-`;
-
 export default function FooterNav() {
   return (
     <Footer>
       <AnchorWrapper>
-        <Anchor to="/impressum">Impressum</Anchor>
-        <Anchor to="/Privacy">Datenschutz</Anchor>
+        <FooterLink to="/impressum">Impressum</FooterLink>
+        <FooterLink to="/Privacy">Datenschutz</FooterLink>
       </AnchorWrapper>
       <BottomLink to="/attendees">Orga-Login</BottomLink>
     </Footer>
