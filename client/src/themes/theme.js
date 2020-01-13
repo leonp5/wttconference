b632ab0b26f9cae3e699f33a8f48e71f454dc361
leonp5 <leonpe@web.de>
2020-01-13 20:39:13 +0100

    Change main color

--- client/src/themes/theme.js
+++ client/src/themes/theme.js
@@ -4,7 +4,7 @@ export const theme = {
     transparentBackground: "rgba(0,0,0, 0.6)",
     transparentBackgroundLight: "rgba(199, 199, 199, 0.5)",
     hover: "rgba(176, 176, 176, 0.5)",
-    primary: "#6E0101",
+    primary: "#4363ac",
     secondary: "#FFFFFF",
     tertiary: "#000000",
     actioncolor: "#5027D8",
