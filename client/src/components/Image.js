a91a1e861b5caf7e7af17b2da972232a6bccb1f0
leonp5 <leonpe@web.de>
2020-01-13 20:16:17 +0100

    Delete unused images

--- client/src/components/Image.js
+++ client/src/components/Image.js
@@ -4,10 +4,9 @@ import { MediaQuery } from "./MediaQueries";
 
 export const HomeTopImage = styled.img`
   width: 100%;
-  /* height: 300px;
-  width: 100%;
+  height: 300px;
   object-fit: cover;
-  object-position: top center; */
+  object-position: top center;
 `;
 
 export const ContactImage = styled.img`
