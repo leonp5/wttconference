a29a102af87f407a75f9c67a97136e65b2a7a5e7
leonp5 <leonpe@web.de>
2020-01-14 21:38:10 +0100

    Add text to conferencefee page

--- client/src/pages/ConferenceFee.js
+++ client/src/pages/ConferenceFee.js
@@ -1,12 +1,44 @@
 import React from "react";
+import styled from "@emotion/styled";
 
-import { Heading } from "../components/Text";
+import { Heading, Text, Heading3 } from "../components/Text";
 import { ContentWrapper } from "../components/Container/ContentWrapper";
+
+const Wrapper = styled(ContentWrapper)`
+  max-width: 800px;
+`;
+
+const Headline = styled(Heading3)`
+  text-align: left;
+  width: 100%;
+  padding-left: 15px;
+  margin-bottom: 0px;
+`;
 
 export default function Contact() {
   return (
-    <ContentWrapper>
+    <Wrapper>
       <Heading>Tagungsbeitrag</Heading>
-    </ContentWrapper>
+      <Text>
+        Der Tagungsbetrag beträgt 20 Euro für Verpflegung und Unterkunft. Dieser Preis wird durch
+        Spenden verschiedener Sponsoren ermöglicht, was bedeutet, dass die realen Kosten pro
+        Teilnehmer*in höher sind. Grundsätzlich soll die Teilnahme aber bei keinem*r Teilnehmer*in
+        durch den finanziellen Beitrag verwehrt bleiben. Kontaktiert daher bei finanziellen Fragen
+        bitte vertrauensvoll das Tagungsbüro (Link zum Tagungsbüro, Punkt 6.), damit wir uns auf
+        eine Ermäßigung einigen können.
+      </Text>
+      <Headline>Konto:</Headline>
+      <Text>
+        Trägergesellschaft für geisteswissenschaftliche Bildung gemeinnützige GmbH <br />
+        Sparkasse Rhein Neckar Nord <br />
+        IBAN DE25 6705 0505 0030 1924 51 <br /> <b>Verwendungszweck:</b> „Studierendentagung“
+      </Text>
+      <Headline>Tagungsort:</Headline>
+      <Text>
+        Alanus Hochschule – Institut für Waldorfpädagogik, Inklusion und Interkulturalität <br />
+        Am Exerzierplatz 21
+        <br /> D-68167 Mannheim
+      </Text>
+    </Wrapper>
   );
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,6 +5,10 @@ import { Heading, Heading2, Text } from "../components/Text";
 import { ContentWrapper } from "../components/Container/ContentWrapper";
 import { HomeTopImage } from "../components/Images";
 
+const Wrapper = styled(ContentWrapper)`
+  max-width: 800px;
+`;
+
 const RelativeContainer = styled.div`
   position: relative;
   width: 100%;
@@ -30,18 +34,20 @@ function Home() {
           Im Institut für Waldorfpädagogik in Mannheim
         </ImageText>
       </RelativeContainer>
-      <Heading>Willkommen!</Heading>
-      <Text>
-        Willkommen auf der Tagungsseite! <br />
-        Hier findest du alle Informationen zur Tagung, zum Programm und hier kannst dich anmelden.
-        <br />
-        Diese Tagung wird von Studierenden für Studierende aller Lehrerseminare/Hochschulen der
-        Anthroposophie und Waldorfpädagogik in Deutschland organisiert. <br />
-        Idee und Ziel dieser Tagung ist es uns gegenseitig kennenzulernen und zu vernetzen.
-        Weiterführend, als Lehrer*innen der Zukunft, zu wissen, wie wir die Herausforderungen im
-        Unterrichten der kommenden Generationen nehmen und wie wir uns gemeinsam daraufhin
-        ausrichten können?
-      </Text>
+      <Wrapper>
+        <Heading>Willkommen!</Heading>
+        <Text>
+          Willkommen auf der Tagungsseite! <br />
+          Hier findest du alle Informationen zur Tagung, zum Programm und hier kannst dich anmelden.
+          <br />
+          Diese Tagung wird von Studierenden für Studierende aller Lehrerseminare/Hochschulen der
+          Anthroposophie und Waldorfpädagogik in Deutschland organisiert. <br />
+          Idee und Ziel dieser Tagung ist es uns gegenseitig kennenzulernen und zu vernetzen.
+          Weiterführend, als Lehrer*innen der Zukunft, zu wissen, wie wir die Herausforderungen im
+          Unterrichten der kommenden Generationen nehmen und wie wir uns gemeinsam daraufhin
+          ausrichten können?
+        </Text>
+      </Wrapper>
     </ContentWrapper>
   );
 }
