a82647142e44ce99fd796ecfabd2ad28470cfc0d
leonp5 <leonpe@web.de>
2020-01-15 12:39:23 +0100

    Add radiobuttons, change style of forms, add labels to form

---
+++ client/src/components/Container/RadioWrapper.js
@@ -0,0 +1,8 @@
+import styled from "@emotion/styled";
+
+export const RadioWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-bottom: 5px;
+`;
--- client/src/components/InputFields.js
+++ client/src/components/InputFields.js
@@ -4,7 +4,6 @@ export const BasicInput = styled.input`
   width: 250px;
   background: ${props => props.theme.colors.background};
   border: none;
-  margin-top: 15px;
   padding: 10px;
 `;
 
