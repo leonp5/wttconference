5fff2c967d967003290f3bbdbe142a0aeb1c575c
leonp5 <leonpe@web.de>
2020-02-03 18:11:45 +0100

    Remoe unused fetch

--- client/src/api/attendees.js
+++ client/src/api/attendees.js
@@ -7,16 +7,3 @@ export function saveAttendee(attendee) {
     body: JSON.stringify(attendee)
   });
 }
-
-export async function getAttendees() {
-  return fetch(`api/attendees/`, {
-    method: "GET"
-  })
-    .then(response => {
-      if (response.status !== 200) {
-        throw new Error(response.statusText);
-      }
-      return response;
-    })
-    .then(response => response.json());
-}
