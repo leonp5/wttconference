efe4d3ff04f804327d3a37ac6003edecc41faa75
leonp5 <leonpe@web.de>
2020-02-05 09:47:00 +0100

    Add workshop routes at the backend Correct login auth route Add saveData function to workshop page

--- backend/routes/dbRoutes.js
+++ backend/routes/dbRoutes.js
@@ -3,6 +3,7 @@ const dbRoutes = express.Router();
 
 const verifyToken = require("../middleware/verifyToken");
 const { addAttendee, getAttendees } = require("../lib/attendees");
+const { getWorkshops, addWorkshops } = require("../lib/workshops");
 
 dbRoutes.get("/attendees", verifyToken, async (request, response) => {
   try {
@@ -19,6 +20,28 @@ dbRoutes.post("/attendees", async (request, response) => {
   try {
     const attendee = request.body;
     await addAttendee(attendee);
+    response.end();
+  } catch (error) {
+    console.error(error);
+    response.status(500).end();
+  }
+});
+
+dbRoutes.get("/workshops", verifyToken, async (request, response) => {
+  try {
+    const workshops = await getWorkshops();
+
+    response.json(workshops);
+  } catch (error) {
+    console.error(error);
+    response.status(500).end();
+  }
+});
+
+dbRoutes.post("/workshops", async (request, response) => {
+  try {
+    const workshop = request.body;
+    await addWorkshops(workshop);
     response.end();
   } catch (error) {
     console.error(error);
