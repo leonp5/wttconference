fe36f0dd8575223cdbf32466d0eb99aafa03b0ac
leonp5 <leonpe@web.de>
2020-02-08 15:03:19 +0100

    Add 404page

---
+++ client/src/pages/ErrorPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { Heading2, Text } from "../components/Text";
+
+export default function ErrorPage() {
+  return (
+    <>
+      <Heading2>404 Page not found</Heading2>
+      <Text>Diese Seite gibt es nicht.</Text>{" "}
+    </>
+  );
+}
