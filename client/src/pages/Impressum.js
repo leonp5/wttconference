31d55f7f775d1029e10d2b499410243332dd17de
leonp5 <leonpe@web.de>
2020-02-12 14:37:10 +0100

    Change privacy and impressum page Deactivate registration function for production

--- client/src/pages/Impressum.js
+++ client/src/pages/Impressum.js
@@ -1,50 +1,23 @@
 import React from "react";
 
-import { Heading, Heading2, Text } from "../components/Text";
+import { Heading, Text } from "../components/Text";
 import { ContentLeft } from "../components/Container/ContentLeft";
 
 export default function Impressum() {
   return (
     <ContentLeft>
       <Heading>Impressum</Heading>
-      <Heading2>Angaben gemäß § 5 TMG</Heading2>
       <Text>
-        Bund der Freien Waldorfschulen e.V. <br /> Wagenburgstr. 6 <br />
-        70184 Stuttgart
+        Siehe Impressum des Bund der Freien Waldorfschulen e.V. <br /> <br />
+        <a href="https://www.waldorfschule.de/impressum/#main-content">
+          https://www.waldorfschule.de/impressum/#main-content
+        </a>
       </Text>
       <Text>
-        <strong>Vertreten durch:</strong> <br />
-        <br /> Vorstände des BdFWS: <br />
-        <br />
-        Nele Auschra, Stefan Grosse, Hans-Georg Hutzel, Henning Kullak-Ublick, Thomas
-        Lutze-Rodenbusch, Eva Wörner <br /> <br />
-        Geschäftsführer:
-        <br />
-        Christian Boettger, Christoph Dörsch, Klaus-Peter Freitag, Hans-Georg Hutzel
-      </Text>
-      <Heading2>Kontakt</Heading2>
-      <Text>
-        Telefon: +49 (0)711 21042-0
-        <br />
-        E-Mail: <a href="mailto:bund@waldorfschule.de">bund@waldorfschule.de</a>
-      </Text>
-      <Heading2>Website</Heading2>
-      <Text>
+        Technische Umsetzung der Website: <br />
         Leon Pelzer <br />
         E-Mail: <a href="mailto:leonpe@web.de">leonpe@web.de</a> <br />
         GitHub: <a href="https://github.com/leonp5">https://github.com/leonp5</a>
-      </Text>
-
-      <Heading2>EU-Streitschlichtung</Heading2>
-      <Text>
-        Die Europäische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit:{" "}
-        <a href=" https://ec.europa.eu/consumers/odr">https://ec.europa.eu/consumers/odr</a>. Unsere
-        E-Mail-Adresse finden Sie oben im Impressum.
-      </Text>
-      <Heading2>Verbraucherstreitbeilegung / Universalschlichtungsstelle</Heading2>
-      <Text>
-        Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer
-        Verbraucherschlichtungsstelle teilzunehmen.
       </Text>
     </ContentLeft>
   );
